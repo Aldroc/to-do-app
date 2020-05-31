1dac0ded4487632734241b6393af6dd46e03542c
Aldroc <riptide1112@gmail.com>
2020-05-31 10:43:04 +0530

    delete_task_implemented

--- src/components/taskList.js
+++ src/components/taskList.js
@@ -10,24 +10,34 @@ class TaskList extends Component{
         this.editClick=this.editClick.bind(this);
         this.state={
             inputStatus: true,
-            renderComponent: false,
+            renderComponent: true,
             currentTaskArray: window.$taskArray.slice()
         }
     }
     deleteClick(){
-    
+    var taskArray=this.state.currentTaskArray.slice();
+    const arrayIndex=taskArray.length-1;
+    window.$taskArray.splice(arrayIndex,1);
+    this.setState({ renderComponent: false });
     }
     editClick(){
         
     }
     render(){
-        return(
-            <div>
-                <input type="text" defaultValue={this.props.taskName} disabled={this.state.inputStatus} />
-                <button type="button"><FontAwesomeIcon icon={faTrash} onClick={this.deleteClick}/></button>
-                <button type="button" onClick={this.editClick}><FontAwesomeIcon icon={faPen} /></button>
-            </div>
-        );
+        if(this.state.renderComponent){
+            return(
+                <div>
+                    <input type="text" defaultValue={this.props.taskName} disabled={this.state.inputStatus} />
+                    <button type="button"><FontAwesomeIcon icon={faTrash} onClick={this.deleteClick}/></button>
+                    <button type="button" onClick={this.editClick}><FontAwesomeIcon icon={faPen} /></button>
+                </div>
+            );
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
     }
 }
 
